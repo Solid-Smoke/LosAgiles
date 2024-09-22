61394029e0a091825f5be4e4d1853b1f13402af0
ANDREW.ROSALES <ANDREW.ROSALES@ucr.ac.cr>
2024-09-22 02:32:25 -0600

    Agregado de rutas Homepage y Registrar Emprendimiento. Eliminacion del Helloworld.vue.

---
+++ front-end/src/components/HomepageView.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="container mt-5">
+        <h1 class="display-4 text-center">Homepage</h1>
+    </div>
+</template>
+
+<script>
+	export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,4 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import { createRouter, createWebHistory } from "vue-router";
+import HomepageView from './components/HomepageView.vue';
+import RegisterBusinessView from './components/RegisterBusinessView.vue';
+
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: "/", name: "Home", component: HomepageView },
+        { path: "/RegistrarEmprendimiento", name: "Register Business", component: RegisterBusinessView },
+    ],
+});
+
+createApp(App).use(router).mount("#app");
