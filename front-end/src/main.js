75e2c1aa0eb3c5255db559c5bc27c998085c22b0
Solid-Smoke <pablo.cascantetorres@ucr.ac.cr>
2024-09-22 14:13:13 -0600

    Branch creation with basic view and route.

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -4,6 +4,7 @@ import App from './App.vue'
 import { createRouter, createWebHistory } from "vue-router";
 import HomepageView from './components/HomepageView.vue';
 import RegisterBusinessView from './components/RegisterBusinessView.vue';
+import RegisterUserView from './components/RegisterUserView.vue';
 
 
 const router = createRouter({
@@ -11,6 +12,7 @@ const router = createRouter({
     routes: [
         { path: "/", name: "Home", component: HomepageView },
         { path: "/RegistrarEmprendimiento", name: "Register Business", component: RegisterBusinessView },
+        { path: "/Registro", name: "Register User", component: RegisterUserView },
     ],
 });
 
