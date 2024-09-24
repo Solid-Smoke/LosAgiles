b0804731d6a5947aba163774ec34beff2d02942a
Jose <jose.amadormora@ucr.ac.cr>
2024-09-24 08:38:43 -0600

    Se corrigen cambios solicitados en el pull request

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,19 +1,14 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import { createRouter, createWebHistory } from "vue-router";
-import AddAddressForm from "./components/AddAddressForm.vue";
-
-import { createRouter, createWebHistory } from "vue-router";
-import HomepageView from './components/HomepageView.vue';
-import RegisterBusinessView from './components/RegisterBusinessView.vue';
-
+import HomepageView from "./components/HomepageView.vue";
+import RegisterBusinessView from "./components/RegisterBusinessView.vue";
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: "/", name: "Home", component: HomepageView },
     { path: "/RegistrarEmprendimiento", name: "Register Business", component: RegisterBusinessView },
-    { path: "/AddressListView/AddAddressForm", name: "AddAddressForm", component: AddAddressForm },
   ],
 });
 
