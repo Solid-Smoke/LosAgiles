f27a6bcc55e58a1aa167f44e0ecae5fab6b1d25c
Jose <jose.amadormora@ucr.ac.cr>
2024-09-28 15:13:02 -0600

    Se borran scripts que no debían subirse desde esta rama

--- scripts/001.sql
+++ scripts/001.sql
@@ -3,24 +3,3 @@ use master
 CREATE DATABASE LosAgilesDB
 
 use LosAgilesDB
-
-CREATE TABLE Clients (
-    UserID INT NOT NULL PRIMARY KEY IDENTITY(1, 1),
-    Name VARCHAR(30),
-    LastNames VARCHAR(60),
-    UserName VARCHAR(30),
-    Email VARCHAR(60),
-    BirthDate DATE,
-    UserPassword VARCHAR(512)
-);
-
-CREATE TABLE ClientsAddresses (
-    AddressID INT NOT NULL PRIMARY KEY IDENTITY(1,1),
-    UserID INT NOT NULL FOREIGN KEY REFERENCES Clients(UserID),
-    Province VARCHAR(20),
-    Canton VARCHAR(40),
-    District VARCHAR(50),
-    PostalCode INT,
-    OtherSigns VARCHAR(500)
-);
-
