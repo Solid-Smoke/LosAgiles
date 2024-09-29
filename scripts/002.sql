a3b12b52389d4f6f89f7248bc5800aee945cb19c
JavierPupo <javi408x@gmail.com>
2024-09-29 17:58:14 -0600

    Se añade el script para crear la tabla de Clientes con una columna nueva; accountState.

---
+++ scripts/002.sql
@@ -0,0 +1,13 @@
+USE LosAgilesDB
+
+CREATE TABLE Clients (
+    UserID INT NOT NULL PRIMARY KEY IDENTITY(1, 1),
+    [Name] VARCHAR(30) NOT NULL,
+    LastNames VARCHAR(60) NOT NULL,
+    UserName VARCHAR(30) NOT NULL,
+    Email VARCHAR(60) NOT NULL,
+    BirthDate DATE NOT NULL,
+    UserPassword VARCHAR(512) NOT NULL,
+	AccountState VARCHAR(10) NOT NULL,
+    UNIQUE(UserName, Email)
+);
