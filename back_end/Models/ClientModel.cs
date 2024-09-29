534058378b8cff33f05b57429610ad2845810dfe
JavierPupo <javi408x@gmail.com>
2024-09-29 17:53:17 -0600

    Se creó el modelo de Clients.

---
+++ back_end/Models/ClientModel.cs
@@ -0,0 +1,14 @@
+﻿namespace back_end.Models
+{
+    public class ClientModel
+    {
+        public int UserID { get; set; }
+        public string Name { get; set; }
+        public string LastNames { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string UserPassword { get; set; }
+        public string AccountState { get; set; }
+    }
+}
