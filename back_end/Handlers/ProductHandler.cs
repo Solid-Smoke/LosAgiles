6c6746d29aab5adaa11aa6983fed58e048f7a400
JavierPupo <javi408x@gmail.com>
2024-09-30 16:25:11 -0600

    Se creó el handler de Product.

---
+++ back_end/Handlers/ProductHandler.cs
@@ -0,0 +1,66 @@
+﻿using back_end.Models;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace back_end.Handlers
+{
+    public class ProductHandler
+    {
+        private SqlConnection _conexion;
+        private string _rutaConexion;
+
+        public ProductHandler()
+        {
+            var builder = WebApplication.CreateBuilder();
+            _rutaConexion = builder.Configuration.GetConnectionString("ClientsContext");
+            _conexion = new SqlConnection(_rutaConexion);
+        }
+
+        private void EjecutarComando(SqlCommand comando)
+        {
+            _conexion.Open();
+            comando.ExecuteNonQuery();
+            _conexion.Close();
+        }
+
+        public bool CrearProducto(ProductModel producto)
+        {
+            // Insertar producto
+            string consultaProducto = @"
+                INSERT INTO Products (Name, Description, Price, Stock, Weight, Perishable, DailyAmount, DaysAvailable, BusinessID)
+                VALUES (@Name, @Description, @Price, @Stock, @Weight, @Perishable, @DailyAmount, @DaysAvailable, @BusinessID);
+                SELECT SCOPE_IDENTITY();";
+
+            SqlCommand comandoProducto = new SqlCommand(consultaProducto, _conexion);
+            comandoProducto.Parameters.AddWithValue("@Name", producto.Name);
+            comandoProducto.Parameters.AddWithValue("@Description", producto.Description);
+            comandoProducto.Parameters.AddWithValue("@Price", producto.Price);
+            comandoProducto.Parameters.AddWithValue("@Stock", producto.Stock);
+            comandoProducto.Parameters.AddWithValue("@Weight", producto.Weight);
+            comandoProducto.Parameters.AddWithValue("@Perishable", producto.Perishable);
+            comandoProducto.Parameters.AddWithValue("@DailyAmount", (object)producto.DailyAmount ?? DBNull.Value);
+            comandoProducto.Parameters.AddWithValue("@DaysAvailable", producto.DaysAvailable);
+            comandoProducto.Parameters.AddWithValue("@BusinessID", producto.BusinessID);
+
+            _conexion.Open();
+            int productId = Convert.ToInt32(comandoProducto.ExecuteScalar());
+            _conexion.Close();
+
+            // Insertar imagen
+            if (producto.ProductImage != null)
+            {
+                string consultaImagen = @"
+                    INSERT INTO Images (Image, ProductID)
+                    VALUES (@Image, @ProductID);";
+
+                SqlCommand comandoImagen = new SqlCommand(consultaImagen, _conexion);
+                comandoImagen.Parameters.AddWithValue("@Image", producto.ProductImage);
+                comandoImagen.Parameters.AddWithValue("@ProductID", productId);
+
+                EjecutarComando(comandoImagen);
+            }
+
+            return true;
+        }
+    }
+}
