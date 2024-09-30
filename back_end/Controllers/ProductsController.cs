210ac5687dc149a998f0c26b2faa9da591bdbd62
JavierPupo <javi408x@gmail.com>
2024-09-30 16:25:04 -0600

    Se creó el controller de Product.

---
+++ back_end/Controllers/ProductsController.cs
@@ -0,0 +1,39 @@
+﻿using back_end.Handlers;
+using back_end.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace back_end.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ProductHandler _productHandler;
+
+        public ProductsController()
+        {
+            _productHandler = new ProductHandler();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<bool>> CrearProducto([FromForm] ProductModel product)
+        {
+            try
+            {
+                if (product == null)
+                {
+                    return BadRequest();
+                }
+
+                var result = _productHandler.CrearProducto(product);
+
+                return new JsonResult(result);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error creando producto");
+            }
+        }
+    }
+}
