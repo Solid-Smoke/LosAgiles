551e857885f8f9fe1cd6f2fd6f5ee43bc9df52c3
ANDREW.ROSALES <ANDREW.ROSALES@ucr.ac.cr>
2024-10-01 00:32:26 -0600

    Creacion de Branch para vista de todos los emprendimientos

--- scripts/003.sql
+++ scripts/003.sql
@@ -9,3 +9,24 @@ CREATE TABLE ClientsAddresses (
     PostalCode INT NOT NULL,
     OtherSigns VARCHAR(500) NOT NULL
 );
+go
+
+CREATE TABLE Businesses (
+    BusinessID INT NOT NULL PRIMARY KEY IDENTITY(1,1),      
+    Name NVARCHAR(50) NOT NULL,              
+    IDNumber NVARCHAR(20) NOT NULL,           
+    Email NVARCHAR(50) NOT NULL,             
+    Telephone NVARCHAR(20) NOT NULL,          
+    Permissions NVARCHAR(200),         
+);
+go
+
+CREATE TABLE BusinessesAddresses (
+    BusinessID INT NOT NULL PRIMARY KEY FOREIGN KEY REFERENCES Businesses(BusinessID),
+    Province NVARCHAR(20) NOT NULL,
+    Canton NVARCHAR(40) NOT NULL,
+    District NVARCHAR(40) NOT NULL,
+    PostalCode NVARCHAR(10) NOT NULL,
+    OtherSigns VARCHAR(500) NOT NULL
+);
+go
