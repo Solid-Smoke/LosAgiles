ff95d66f7b6c212480421f2db50bcbb1611307bc
Jose <jose.amadormora@ucr.ac.cr>
2024-10-21 09:44:31 -0600

    Reapply "Se empieza feature buscar productos."
    This reverts commit 93019991e88955014d6c63d81393d8862ab71348.

---
+++ back_end/Application/ProductSearchHttpRequestParameterValidator.cs
@@ -0,0 +1,52 @@
+﻿namespace back_end.Application
+{
+    public interface IProductSearchHttpRequestParameterValidator
+    {
+        void checkSearchProductsParameters(string searchText, int startIndex, int maxResults, string filterTypeString, string filter);
+    }
+
+    public class ProductSearchHttpRequestParameterValidator : IProductSearchHttpRequestParameterValidator
+    {
+
+        private readonly IFactoryProductSearchFilter factoryProductSearchFilter;
+
+        public ProductSearchHttpRequestParameterValidator(IFactoryProductSearchFilter factoryProductSearchFilter)
+        {
+            this.factoryProductSearchFilter = factoryProductSearchFilter;
+        }
+
+
+        public void checkSearchProductsParameters(string searchText, int startIndex,
+            int maxResults, string filterTypeString, string filter)
+        {
+            if (searchText == null)
+                throw new ArgumentNullException("seachText parameter is null");
+
+            else if (startIndex < 1)
+                throw new ArgumentOutOfRangeException(
+                    $"Invalid parameter startIndex:" +
+                    $"{startIndex}, should be greather than 1");
+
+            else if (maxResults < 1)
+                throw new ArgumentOutOfRangeException(
+                    $"Invalid parameter maxResults: {maxResults}," +
+                    $"should be greather than 1");
+
+            else if (filterTypeString == null)
+                throw new ArgumentNullException("filterTypeString is null");
+
+            else if (filter == null)
+                throw new ArgumentNullException("filter is null");
+
+            else if (factoryProductSearchFilter.isFilterType(filterTypeString) == false)
+                throw new ArgumentException($"Invalid filter type:" +
+                    $"{filterTypeString}");
+
+            else if (factoryProductSearchFilter
+                .filterIsValid(filterTypeString, filter) == false)
+                throw new ArgumentException(
+                    $"Invalid filter for filter type {filterTypeString}:" +
+                    $"{filter}");
+        }
+    }
+}
---
+++ back_end/Application/ProductSearchLogic.cs
@@ -0,0 +1,32 @@
+﻿using back_end.Handlers;
+using back_end.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace back_end.Application
+{
+    public interface IProductSearchLogic
+    {
+        List<ProductModel> searchProducts(string searchText, int startIndex, int maxResults, string filterTypeString, string filter);
+    }
+
+    public class ProductSearchLogic : IProductSearchLogic
+    {
+        private readonly IProductHandler productHandler;
+        private readonly IFactoryProductSearchFilter factoryProductSearchFilter;
+
+        public ProductSearchLogic(IProductHandler productHandler, IFactoryProductSearchFilter factoryProductSearchFilter)
+        {
+            this.productHandler = productHandler;
+            this.factoryProductSearchFilter = factoryProductSearchFilter;
+        }
+
+
+        public List<ProductModel> searchProducts(string searchText,
+            int startIndex, int maxResults, string filterTypeString, string filter)
+        {
+            return productHandler.searchProducts(searchText, startIndex,
+                maxResults,
+                factoryProductSearchFilter.getFilterType(filterTypeString), filter);
+        }
+    }
+}
