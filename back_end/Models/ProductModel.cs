e2e39074fb680d42aaf76e93936810c562bb194b
JavierPupo <javi408x@gmail.com>
2024-10-22 13:45:03 -0600

    Se cambió la variable Perishable a IsPerishable para que sea compatible con la tabla de la BD. Además se agregó un campo mas para poder enviar la imagaen de vuelta al frontend.

--- back_end/Models/ProductModel.cs
+++ back_end/Models/ProductModel.cs
@@ -1,17 +1,15 @@
-﻿namespace back_end.Models
+﻿public class ProductModel
 {
-    public class ProductModel
-    {
-        public int? ProductID { get; set; }
-        public string? Name { get; set; }
-        public string? Description { get; set; }
-        public int? Price { get; set; }
-        public int? Stock { get; set; }
-        public decimal? Weight { get; set; }
-        public bool? Perishable { get; set; }
-        public int? DailyAmount { get; set; }
-        public string? DaysAvailable { get; set; }
-        public int? BusinessID { get; set; }
-        public byte[]? ProductImage { get; set; } 
-    }
+    public int? ProductID { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public int? Price { get; set; }
+    public int? Stock { get; set; }
+    public decimal? Weight { get; set; }
+    public bool? IsPerishable { get; set; } 
+    public int? DailyAmount { get; set; }
+    public string? DaysAvailable { get; set; }
+    public int? BusinessID { get; set; }
+    public byte[]? ProductImage { get; set; }
+    public string? ProductImageBase64 { get; set; } 
 }
