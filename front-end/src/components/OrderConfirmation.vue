6c4159a0beda3b5d2f985a5a0584ab29db57da16
Solid-Smoke <pablo.cascantetorres@ucr.ac.cr>
2024-10-23 16:21:37 -0600

    Cambio para incluir los campos segun solicito el profe desde de ver el mockup, la dirección se mantiene ambigua hasta que se integre en la respectiva US.

--- front-end/src/components/OrderConfirmation.vue
+++ front-end/src/components/OrderConfirmation.vue
@@ -1,6 +1,6 @@
 <template>
     <template v-if="adminName">
-        <template v-if="adminEmail"> <!-- Asegurarse de cambiar esto, esta asi solo para trabajarlo -->
+        <template v-if="!adminEmail"> <!-- Asegurarse de cambiar esto, esta asi solo para trabajarlo -->
             <h1 class="display-4 text-center mb-4"><strong>403 Forbidden</strong></h1>
             <h3 class="text-center mb-4">Página solo administradores <br /><a href="/">Regresar</a></h3>
         </template>
@@ -14,6 +14,8 @@
                             <th scope="col"># de orden</th>
                             <th scope="col">Comprador</th>
                             <th scope="col">Fecha de creación</th>
+                            <th scope="col">Monto total</th>
+                            <th scope="col">Dirección de entrega</th>
                             <th scope="col">Productos</th>
                             <th scope="col">Acciones</th>
                         </tr>
@@ -21,8 +23,12 @@
                     <tbody>
                         <tr v-for="order in orders" :key="order.orderID">
                             <td>{{ order.orderID }}</td>
-                            <td>{{ order.associatedUser }}</td>
+                            <td>{{ order.userName }} {{ order.userLastName }}</td>
                             <td>{{ order.createdDate }}</td>
+                            <td>{{ order.total }}</td>
+                            <td>
+                                <button v-on:click="showAddress(order)" class="btn btn-info">Ver dirección</button>
+                            </td>
                             <td>
                                 <button v-on:click="showProducts(order)" class="btn btn-info">Ver productos</button>
                             </td>
@@ -35,14 +41,19 @@
                 </table>
             </div>
 
-            <!-- Modal for displaying products -->
             <b-modal v-model="ProductsModal" centered scrollable hide-footer title="Productos de la orden">
                 <template v-if="selectedProducts.length">
                     <ul>
                         <li v-for="product in selectedProducts" :key="product.productID">
-                            {{ product.name }} - Cantidad: {{ product.amount }} - Precio: {{ product.price }}
+                            {{ product.name }} - Cantidad: {{ product.amount }}
                         </li>
                     </ul>
+                </template>
+            </b-modal>
+
+            <b-modal v-model="AddressModal" centered scrollable hide-footer title="Dirección de la orden">
+                <template v-if="true">
+                    <p>Dirección aquí</p>
                 </template>
             </b-modal>
         </template>
@@ -64,26 +75,36 @@
         data() {
             return {
                 ProductsModal: false,
+                AddressModal: false,
                 selectedProducts: [],
+                selectedAddress: null,
                 orders: [
                     {
                         orderID: 1000,
-                        associatedUser: '@maria123', //La idea es recibir el nombre, no el id, se encarga un metodo
+                        userName: 'Maria', //La idea es recibir el nombre, no el id, se encarga un metodo
+                        userLastName: 'Rodriguez',
                         createdDate: '23/10/2024', //Se va a usar para tener un trigger 
+                        total: 35,
                         state: 'Pendiente',
+                        selectedAction: null,
                         products: [ //Se saca del join entre orders > OrderProducts > Products
-                            { productID: 1, name: 'Producto A', amount: 2, price: 10 },
-                            { productID: 3, name: 'Producto C', amount: 1, price: 15 },
+                            { productID: 1, name: 'Producto A', amount: 2 },
+                            { productID: 3, name: 'Producto C', amount: 1 },
                         ],
+                        address: '',
                     },
                     {
                         orderID: 1001,
-                        associatedUser: '@jose124', //La idea es recibir el nombre, no el id, se encarga un metodo
+                        userName: 'Jose', //La idea es recibir el nombre, no el id, se encarga un metodo
+                        userLastName: 'Arias',
                         createdDate: '23/10/2024', //Se va a usar para tener un trigger 
                         state: 'Pendiente',
+                        total: 25,
+                        selectedAction: null,
                         products: [
-                            { productID: 2, name: 'Producto B', amount: 5, price: 5 },
+                            { productID: 2, name: 'Producto B', amount: 5 },
                         ],
+                        address: '',
                     },
                 ],
                 adminName: "",
@@ -102,11 +123,13 @@
                 this.selectedProducts = order.products;
                 this.ProductsModal = true;
             },
+            showAddress(order) {
+                this.selectedAddress = order.address;
+                this.AddressModal = true;
+            },
             //approveOrder(order) {
-            //    // Your approval logic here
             //},
             //rejectOrder(order) {
-            //    // Your rejection logic here
             //},
         },
         created() {
