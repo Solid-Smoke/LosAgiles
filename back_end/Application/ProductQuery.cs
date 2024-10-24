93c59e252ea1dea1ad55da1733728fc2546adca3
JavierPupo <javi408x@gmail.com>
2024-10-23 20:02:50 -0600

    Se crearon los archivos ProductCommand y ProductQuerry dentro de la carpeta Application para cumplir con la arquitectura.

---
+++ back_end/Application/ProductQuery.cs
@@ -0,0 +1,30 @@
+﻿using back_end.Domain;
+using back_end.Repositories;
+
+namespace back_end.Application
+{
+    public class ProductQuery
+    {
+        private readonly ProductHandler _productHandler;
+
+        public ProductQuery()
+        {
+            _productHandler = new ProductHandler();
+        }
+
+        public List<ProductModel> GetAllProducts()
+        {
+            var products = _productHandler.GetAllProducts();
+
+            foreach (var product in products)
+            {
+                if (product.ProductImage != null)
+                {
+                    product.ProductImageBase64 = Convert.ToBase64String(product.ProductImage);
+                }
+            }
+
+            return products;
+        }
+    }
+}
