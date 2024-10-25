268ac2e59dd4c9ebb1cf08e15bdbae8c7011c4a6
Solid-Smoke <pablo.cascantetorres@ucr.ac.cr>
2024-10-24 19:24:56 -0600

    Cambio de metodo de detección de rol de usuario con la forma nueva, es un ejemplo de su uso

--- front-end/src/components/OrderConfirmation.vue
+++ front-end/src/components/OrderConfirmation.vue
@@ -1,62 +1,56 @@
 <template>
-    <template v-if="adminName">
-        <template v-if="!adminEmail"> <!-- Asegurarse de cambiar esto, esta asi solo para trabajarlo -->
-            <h1 class="display-4 text-center mb-4"><strong>403 Forbidden</strong></h1>
-            <h3 class="text-center mb-4">Página solo administradores <br /><a href="/">Regresar</a></h3>
-        </template>
-        <template v-else>
-            <MainNavbar />
-            <h1 class="display-4 text-center mb-4"><strong>Revisión de ordenes</strong></h1>
-            <div class="table-responsive-sm">
-                <table class="table table-striped table-hover">
-                    <thead>
-                        <tr>
-                            <th scope="col"># de orden</th>
-                            <th scope="col">Comprador</th>
-                            <th scope="col">Fecha de creación</th>
-                            <th scope="col">Monto total</th>
-                            <th scope="col">Dirección de entrega</th>
-                            <th scope="col">Productos</th>
-                            <th scope="col">Acciones</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr v-for="order in orders" :key="order.orderID">
-                            <td>{{ order.orderID }}</td>
-                            <td>{{ order.userName }} {{ order.userLastName }}</td>
-                            <td>{{ order.createdDate }}</td>
-                            <td>{{ order.total }}</td>
-                            <td>
-                                <button v-on:click="showAddress(order)" class="btn btn-info">Ver dirección</button>
-                            </td>
-                            <td>
-                                <button v-on:click="showProducts(order)" class="btn btn-info">Ver productos</button>
-                            </td>
-                            <td>
-                                <button v-on:click="approveOrder(order)" class="btn btn-success">Aprobar</button> <span />
-                                <button v-on:click="rejectOrder(order)" class="btn btn-danger">Rechazar</button>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
+    <template v-if="isAdmin">
+        <MainNavbar />
+        <h1 class="display-4 text-center mb-4"><strong>Revisión de ordenes</strong></h1>
+        <div class="table-responsive-sm">
+            <table class="table table-striped table-hover">
+                <thead>
+                    <tr>
+                        <th scope="col"># de orden</th>
+                        <th scope="col">Comprador</th>
+                        <th scope="col">Fecha de creación</th>
+                        <th scope="col">Monto total</th>
+                        <th scope="col">Dirección de entrega</th>
+                        <th scope="col">Productos</th>
+                        <th scope="col">Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="order in orders" :key="order.orderID">
+                        <td>{{ order.orderID }}</td>
+                        <td>{{ order.userName }} {{ order.userLastName }}</td>
+                        <td>{{ order.createdDate }}</td>
+                        <td>{{ order.total }}</td>
+                        <td>
+                            <button v-on:click="showAddress(order)" class="btn btn-info">Ver dirección</button>
+                        </td>
+                        <td>
+                            <button v-on:click="showProducts(order)" class="btn btn-info">Ver productos</button>
+                        </td>
+                        <td>
+                            <button v-on:click="approveOrder(order)" class="btn btn-success">Aprobar</button> <span />
+                            <button v-on:click="rejectOrder(order)" class="btn btn-danger">Rechazar</button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
 
-            <b-modal v-model="ProductsModal" centered scrollable hide-footer title="Productos de la orden">
-                <template v-if="selectedProducts.length">
-                    <ul>
-                        <li v-for="product in selectedProducts" :key="product.productID">
-                            {{ product.name }} - Cantidad: {{ product.amount }}
-                        </li>
-                    </ul>
-                </template>
-            </b-modal>
+        <b-modal v-model="ProductsModal" centered scrollable hide-footer title="Productos de la orden">
+            <template v-if="selectedProducts.length">
+                <ul>
+                    <li v-for="product in selectedProducts" :key="product.productID">
+                        {{ product.name }} - Cantidad: {{ product.amount }}
+                    </li>
+                </ul>
+            </template>
+        </b-modal>
 
-            <b-modal v-model="AddressModal" centered scrollable hide-footer title="Dirección de la orden">
-                <template v-if="true">
-                    <p>Dirección aquí</p>
-                </template>
-            </b-modal>
-        </template>
+        <b-modal v-model="AddressModal" centered scrollable hide-footer title="Dirección de la orden">
+            <template v-if="true">
+                <p>Dirección aquí</p>
+            </template>
+        </b-modal>
     </template>
     <template v-else>
         <h1 class="display-4 text-center mb-4"><strong>403 Forbidden</strong></h1>
@@ -107,18 +101,9 @@
                         address: '',
                     },
                 ],
-                adminName: "",
-                adminEmail: "",
             };
         },
         methods: {
-            getAdminData() {
-                const user = JSON.parse(localStorage.getItem('user'));
-                if (user && user.length > 0) {
-                    this.adminName = user[0].userName;
-                    this.adminEmail = user[0].email;
-                }
-            },
             showProducts(order) {
                 this.selectedProducts = order.products;
                 this.ProductsModal = true;
@@ -132,8 +117,15 @@
             //rejectOrder(order) {
             //},
         },
-        created() {
-            this.getAdminData();
+        props: {
+            isAdmin: {
+                type: Boolean,
+                required: true,
+            },
+            isClient: {
+                type: Boolean,
+                required: true,
+            },
         },
     }
 </script>
