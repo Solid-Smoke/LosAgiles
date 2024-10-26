cd7ef4eb122501e5f2846d8fb665f691e199bc24
Solid-Smoke <pablo.cascantetorres@ucr.ac.cr>
2024-10-26 17:48:59 -0600

    Agregado de query de productos por orden

--- back_end/Application/Interfaces/IOrderHandler.cs
+++ back_end/Application/Interfaces/IOrderHandler.cs
@@ -3,6 +3,7 @@
 namespace back_end.Application.Interfaces {
     public interface IOrderHandler {
         List<OrderModel> GetPendingOrders();
+        List<OrderProductsModel> GetProductsByOrderID(string OrderID);
         bool ApproveOrder(string OrderID);
         bool RejectOrder(string OrderID);
     }
---
+++ back_end/Application/Queries/GetProductsByOrderID.cs
@@ -0,0 +1,17 @@
+﻿using back_end.Application.Interfaces;
+using back_end.Domain;
+using back_end.Infrastructure.Repositories;
+namespace back_end.Application.Queries {
+    public class GetProductsByOrderID {
+        private readonly IOrderHandler _orderHandler;
+
+        public GetProductsByOrderID(IOrderHandler orderHandler) {
+            _orderHandler = orderHandler;
+        }
+
+        public List<OrderProductsModel> Execute(string OrderId) {
+            // Llamar al método GetProductsByOrderID del repositorio
+            return _orderHandler.GetProductsByOrderID(OrderId);
+        }
+    }
+}
