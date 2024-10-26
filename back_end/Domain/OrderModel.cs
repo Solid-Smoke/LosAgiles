fba54f30980e3846c54583bdb666f0c8b42aadbe
Solid-Smoke <pablo.cascantetorres@ucr.ac.cr>
2024-10-25 23:32:23 -0600

    Modelos de orden y productos en orden.

---
+++ back_end/Domain/OrderModel.cs
@@ -0,0 +1,10 @@
+﻿namespace back_end.Domain {
+    public class OrderModel {
+        public int? OrderID { get; set; }
+        public string? Status { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? DeliveryDate { get; set; }
+        public int? ClientID { get; set; }
+        public int? DeliveryAddress { get; set; }
+    }
+}
---
+++ back_end/Domain/OrderProductsModel.cs
@@ -0,0 +1,7 @@
+﻿namespace back_end.Domain {
+    public class OrderProductsModel {
+        public int? OrderID { get; set; }
+        public int? ProductID { get; set; }
+        public int? Amount { get; set; }
+    }
+}
