211ba8f2295ff7b0d74609bf8267ab3784a87838
JavierPupo <javi408x@gmail.com>
2024-10-26 12:18:35 -0600

    Se arregló que la vista de usuarios registrados solo la puedan ver admins.

--- front-end/src/components/MainNavbar.vue
+++ front-end/src/components/MainNavbar.vue
@@ -26,6 +26,7 @@
 
                     <b-nav-item-dropdown text="Admin">
                         <b-dropdown-item href="/AdminViewAllBusiness">Ver todas las empresas registradas</b-dropdown-item>
+                        <b-dropdown-item href="/UserList">Ver todos los usuarios</b-dropdown-item>
                     </b-nav-item-dropdown>
                 </b-navbar-nav>
 
