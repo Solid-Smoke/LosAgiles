a73a352af30a2e61a5aa9229fdebf934dc9344af
Solid-Smoke <pablo.cascantetorres@ucr.ac.cr>
2024-10-26 17:47:04 -0600

    Actualizacion de Modelos

--- back_end/Domain/OrderModel.cs
+++ back_end/Domain/OrderModel.cs
@@ -6,5 +6,8 @@
         public DateTime? DeliveryDate { get; set; }
         public int? ClientID { get; set; }
         public int? DeliveryAddress { get; set; }
+        public string? Buyer { get; set; }
+        public int? TotalAmount { get; set; }
+        public string? Address { get; set; }
     }
 }
--- back_end/Domain/OrderProductsModel.cs
+++ back_end/Domain/OrderProductsModel.cs
@@ -3,5 +3,6 @@
         public int? OrderID { get; set; }
         public int? ProductID { get; set; }
         public int? Amount { get; set; }
+        public string? ProductName { get; set; }
     }
 }
