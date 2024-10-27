5476b1b56568e576269507e3302f6af4e531fe4a
Solid-Smoke <pablo.cascantetorres@ucr.ac.cr>
2024-10-27 15:23:46 -0600

    Back-end de la feature parcialmente implementado.

---
+++ back_end/Application/Queries/GetOrdersByClientID.cs
@@ -0,0 +1,16 @@
+﻿using back_end.Application.Interfaces;
+using back_end.Domain;
+using back_end.Infrastructure.Repositories;
+namespace back_end.Application.Queries {
+    public class GetOrdersByClientID {
+        private readonly IOrderHandler _orderHandler;
+
+        public GetOrdersByClientID(IOrderHandler orderHandler) {
+            _orderHandler = orderHandler;
+        }
+
+        public List<OrderModel> Execute(string ClientId) {
+            return _orderHandler.GetOrdersByClientID(ClientId);
+        }
+    }
+}
--- back_end/Infrastructure/Repositories/OrderHandler.cs
+++ back_end/Infrastructure/Repositories/OrderHandler.cs
@@ -44,6 +44,38 @@ namespace back_end.Infrastructure.Repositories {
             return OrderData;
         }
 
+        public List<OrderModel> GetOrdersByClientID(string clientID) {
+            List<OrderModel> OrderData = new List<OrderModel>();
+            string query = ""; // missing will be put in a bit
+            try {
+                using (SqlCommand sqlCommand = new SqlCommand(query, sqlConnection)) {
+                    sqlCommand.Parameters.AddWithValue("@ClientID", clientID);
+                    sqlConnection.Open();
+
+                    using (SqlDataReader reader = sqlCommand.ExecuteReader()) {
+                        while (reader.Read()) {
+                            OrderData.Add(
+                                new OrderModel {
+                                    OrderID = Convert.ToInt32(reader["OrderID"]), //These have to change
+                                    Buyer = reader["Buyer"].ToString(),
+                                    CreatedDate = Convert.ToDateTime(reader["CreatedDate"]),
+                                    TotalAmount = Convert.ToInt32(reader["TotalAmount"]),
+                                    Address = reader["Address"].ToString()
+                                });
+                        }
+                    }
+                    sqlConnection.Close();
+                }
+            }
+            catch (SqlException sqlEx) {
+                Console.WriteLine($"SQL Error: {sqlEx.Message}");
+            }
+            catch (Exception ex) {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+            return OrderData;
+        }
+
         public List<OrderProductsModel> GetProductsByOrderID(string orderID) {
             List<OrderProductsModel> OrderProductsData = new List<OrderProductsModel>();
             string query = "SELECT * FROM [udfProductsByOrderID](@OrderID)";
