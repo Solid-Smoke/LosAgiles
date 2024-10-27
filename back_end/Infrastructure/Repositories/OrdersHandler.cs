9de1d5b3df8893df9fb2a754d4132dbcf3d7b2f6
Jose <jose.amadormora@ucr.ac.cr>
2024-10-27 16:07:28 -0600

    Ya se colocan las orders con el cambio del tipo de ID de Orders
    # Conflicts:
    #	back_end/Domain/OrderModel.cs

--- back_end/Infrastructure/Repositories/OrdersHandler.cs
+++ back_end/Infrastructure/Repositories/OrdersHandler.cs
@@ -18,48 +18,52 @@ namespace back_end.Infrastructure.Repositories
         {
             sqlConnection.Open();
             sqlConnection.Execute("BEGIN TRAN");
-            Guid orderID = sqlConnection.Query<Guid>("SELECT newid()").ToList<Guid>()[0];
-            bool insertedInOrders = insertInOrders(orderData.ClientID, orderID, orderData.DeliveryAddress);
-            bool insertedInOrderProducts = insertInOrderProducts(orderID, orderData.Products);
-            bool InsertedInBusinessOrders = insertInBusinessOrders(orderID, orderData.Products);
-            if (insertedInOrders && insertedInOrderProducts && InsertedInBusinessOrders)
+            try
             {
+                int? orderID = insertInOrders(orderData.ClientID, orderData.DeliveryAddress);
+                if (orderID == null)
+                    throw new Exception("Inserted OrderID is null");
+                Console.WriteLine("Inserted orderID in transaction: " + orderID);
+                bool insertedInOrderProducts = insertInOrderProducts((int)orderID, orderData.Products);
+                bool InsertedInBusinessOrders = insertInBusinessOrders((int)orderID, orderData.Products);
                 sqlConnection.Execute("COMMIT");
                 sqlConnection.Close();
                 return true;
             }
-            else
+            catch (Exception ex)
             {
                 sqlConnection.Execute("ROLLBACK");
                 sqlConnection.Close();
-                return false;
+                throw new Exception(ex.ToString());
             }
         }
 
-        private bool insertInBusinessOrders(Guid orderID, List<OrderProductsModel> products)
+        private bool insertInBusinessOrders(int orderID, List<OrderProductsModel> products)
         {
             var insertList = new List<object>();
             foreach (var product in products)
                 insertList.Add(new { orderID, product.BusinessID });
             insertList = insertList.Distinct<object>().ToList();
-            return sqlConnection.Execute("INSERT INTO BusinessOrders VALUES (@orderID, @businessID)", insertList) > 0;
+            return sqlConnection.Execute("INSERT INTO BusinessOrders (OrderID, BusinessID) VALUES (@orderID, @BusinessID)", insertList) > 0;
         }
 
-        private bool insertInOrderProducts(Guid orderID, List<OrderProductsModel> products)
+        private bool insertInOrderProducts(int orderID, List<OrderProductsModel> products)
         {
             var insertList = new List<object>();
             foreach (var product in products)
                 insertList.Add(new { product.ProductID, orderID, product.Ammount });
-            return sqlConnection.Execute("INSERT INTO OrderProducts VALUES (@productId, @orderID, @ammount)", insertList) > 0;
+            return sqlConnection.Execute("INSERT INTO OrderProducts (ProductId, OrderID, Amount) VALUES (@ProductId, @orderID, @Ammount)", insertList) > 0;
         }
 
-        private bool insertInOrders(int clientID, Guid orderID, ClientsAddress deliveryAddress)
+        private int insertInOrders(int? clientID, ClientsAddress deliveryAddress)
         {
-            return sqlConnection.Execute(
-                "INSERT INTO Orders (OrderID, [Status], CreatedDate, ClientID, DeliveryAddress)\r\n" +
-                "VALUES (@orderID, 'Submitted', getdate(), @clientID, @deliveryAddressID)",
-                new { orderID, clientID, deliveryAddressID = deliveryAddress.AddressID }
-            ) > 0;
+
+            return sqlConnection.QuerySingle<int>(
+                "INSERT INTO Orders (CreatedDate, ClientID, DeliveryAddress)\r\n" +
+                "OUTPUT INSERTED.OrderID\r\n" +
+                "VALUES (getdate(), @clientID, @deliveryAddressID)",
+                new { clientID, deliveryAddressID = deliveryAddress.AddressID }
+            );
         }
     }
 }
