ee8e1c981ff019741536cb4feaed841069133d8a
Solid-Smoke <pablo.cascantetorres@ucr.ac.cr>
2024-10-27 21:17:44 -0600

    Error menor de indentacion resuelto.

--- front-end/src/components/CartView.vue
+++ front-end/src/components/CartView.vue
@@ -98,170 +98,170 @@
     </template>
 </template>
 
-    <script>
-        import MainNavbar from './MainNavbar.vue';
-        import ActionModalConfirm from './ActionModalConfirm.vue';
-        import ActionModalWarning from './ActionModalWarning.vue';
-        import ActionModalError from './ActionModalError.vue';
-        import { BackendUrl } from '../main.js';
-        import axios from "axios";
+<script>
+    import MainNavbar from './MainNavbar.vue';
+    import ActionModalConfirm from './ActionModalConfirm.vue';
+    import ActionModalWarning from './ActionModalWarning.vue';
+    import ActionModalError from './ActionModalError.vue';
+    import { BackendUrl } from '../main.js';
+    import axios from "axios";
 
-        export default {
-            components: {
-                MainNavbar,
-                ActionModalConfirm,
-                ActionModalWarning,
-                ActionModalError,
-            },
-            data() {
-                return {
-                    cartProducts: [
-                        {
-                            productID: 1,
-                            productName: 'Producto 1',
-                            businessName: 'Emprendimiento A',
-                            amount: 2,
-                            price: 1000,
-                            totalSales: 2000,
-                        },
-                        {
-                            productID: 2,
-                            productName: 'Producto 2',
-                            businessName: 'Emprendimiento A',
-                            amount: 2,
-                            price: 1000,
-                            totalSales: 2000,
-                        },
-                    ],
-                    selectedProducts: [],
-                    invalidProducts: [],
-                    userID: 0,
-                    hasProducts: false,
-                };
-            },
-            methods: {
-                getUserCart() {
-                    axios.get(`${BackendUrl}/ShoppingCart/${this.userID}`)
-                        .then((response) => {
-                            this.cartProducts = response.data;
-                            if (this.cartProducts[0]) {
-                                this.hasProducts = true;
-                            }
-                        })
-                        .catch((error) => {
-                            this.$refs.errorCartModal.openModal("Error al cargar el carrito", error);
-                        });
-                },
-                closeCart() {
-                    this.$router.push({ name: 'Home' });
-                },
-                toggleSelect(product) {
-                    const productIndex =
-                        this.selectedProducts.findIndex(selected => selected.productID === product.productID);
-                    if (productIndex > -1) {
-                        this.selectedProducts.splice(productIndex, 1);
-                    } else {
-                        this.selectedProducts.push({
-                            productID: product.productID,
-                            productName: product.productName,
-                            businessName: product.businessName,
-                            amount: product.amount,
-                            price: product.price,
-                            totalSales: product.totalSales
-                        });
-                    }
-                },
-                getUserId() {
-                    const user = JSON.parse(localStorage.getItem('user'));
-                    return Number(user[0].userID);
-                },
-                checkout() {
-                    this.VerifyCartItems();
-                },
-                openCleanCartWarningModal() {
-                    this.$refs.warningCleanCartModal.openModal("¿Estás seguro de que deseas vaciar el carrito? (Esta accion es irreversible)");
-                },
-                clearCart() {
-                    axios.delete(`${BackendUrl}/ShoppingCart/${this.userID}`)
-                        .then(() => {
-                            this.$refs.confirmCartModal.openModal("Carrito vaciado");
-                            this.cartProducts = [];
-                        })
-                        .catch((error) => {
-                            this.$refs.errorCartModal.openModal("Error al eliminar el carrito", error);
-                        });
-                },
-                toggleSelectAll(event) {
-                    if (event.target.checked) {
-                        this.selectedProducts = this.cartProducts.map(product => ({
-                            productID: product.productID,
-                            quantity: product.quantity,
-                            total: product.total,
-                        }));
-                    } else {
-                        this.selectedProducts = [];
-                    }
-                },
-                VerifyCartItems() {
-                    axios.get(`${BackendUrl}/ShoppingCart/${this.userID}/Verify`)
-                        .then((response) => {
-                            this.invalidItems = response.data;
-                            if (this.invalidItems.length > 0) {
-                                this.$refs.warningInvalidCartModal.openModal("Se han detectado items invalidos (Exceden el stock disponible) ¿Desea eliminarlos del carrito?");
-                            } else {
-                                this.$refs.confirmCartModal.openModal("Su carrito es valido, se puede procesar la compra");
-                            }
-                        })
-                        .catch((error) => {
-                            this.$refs.errorCartModal.openModal("Error al verificar los productos del carrito", error);
-                        });
-
-
-                },
-                deleteInvalidItems() {
-                    axios.delete(`${BackendUrl}/ShoppingCart/${this.userID}/DeleteInvalidProducts`, {
-                        data: this.invalidItems
-                    }).then(() => {
-                        this.$refs.confirmCartModal.openModal("Se han eliminado los elementos invalidos del carrito");
-                        this.getUserCart();
+    export default {
+        components: {
+            MainNavbar,
+            ActionModalConfirm,
+            ActionModalWarning,
+            ActionModalError,
+        },
+        data() {
+            return {
+                cartProducts: [
+                    {
+                        productID: 1,
+                        productName: 'Producto 1',
+                        businessName: 'Emprendimiento A',
+                        amount: 2,
+                        price: 1000,
+                        totalSales: 2000,
+                    },
+                    {
+                        productID: 2,
+                        productName: 'Producto 2',
+                        businessName: 'Emprendimiento A',
+                        amount: 2,
+                        price: 1000,
+                        totalSales: 2000,
+                    },
+                ],
+                selectedProducts: [],
+                invalidProducts: [],
+                userID: 0,
+                hasProducts: false,
+            };
+        },
+        methods: {
+            getUserCart() {
+                axios.get(`${BackendUrl}/ShoppingCart/${this.userID}`)
+                    .then((response) => {
+                        this.cartProducts = response.data;
+                        if (this.cartProducts[0]) {
+                            this.hasProducts = true;
+                        }
                     })
-                        .catch((error) => {
-                            this.$refs.errorCartModal.openModal("Error al eliminar los elementos invalidos del carrito", error);
-                        });
+                    .catch((error) => {
+                        this.$refs.errorCartModal.openModal("Error al cargar el carrito", error);
+                    });
+            },
+            closeCart() {
+                this.$router.push({ name: 'Home' });
+            },
+            toggleSelect(product) {
+                const productIndex =
+                    this.selectedProducts.findIndex(selected => selected.productID === product.productID);
+                if (productIndex > -1) {
+                    this.selectedProducts.splice(productIndex, 1);
+                } else {
+                    this.selectedProducts.push({
+                        productID: product.productID,
+                        productName: product.productName,
+                        businessName: product.businessName,
+                        amount: product.amount,
+                        price: product.price,
+                        totalSales: product.totalSales
+                    });
                 }
             },
-            computed: {
-                totalPrice() {
-                    return this.cartProducts.reduce((total, product) => total + product.totalSales, 0);
-                },
-                selectedTotal() {
-                    if (this.selectedProducts.length > 0) {
-                        return this.selectedProducts.reduce((total, product) => total + product.totalSales, 0);
-                    }
-                    return 0;
-                },
-                areAllSelected() {
-                    return this.selectedProducts.length === this.cartProducts.length;
-                },
+            getUserId() {
+                const user = JSON.parse(localStorage.getItem('user'));
+                return Number(user[0].userID);
             },
-            mounted() {
-                this.userID = this.getUserId();
-                this.getUserCart();
+            checkout() {
+                this.VerifyCartItems();
             },
-            props: {
-                isAdmin: {
-                    type: Boolean,
-                    required: true,
-                },
-                isClient: {
-                    type: Boolean,
-                    required: true,
-                },
+            openCleanCartWarningModal() {
+                this.$refs.warningCleanCartModal.openModal("¿Estás seguro de que deseas vaciar el carrito? (Esta accion es irreversible)");
             },
-        };
-    </script>
+            clearCart() {
+                axios.delete(`${BackendUrl}/ShoppingCart/${this.userID}`)
+                    .then(() => {
+                        this.$refs.confirmCartModal.openModal("Carrito vaciado");
+                        this.cartProducts = [];
+                    })
+                    .catch((error) => {
+                        this.$refs.errorCartModal.openModal("Error al eliminar el carrito", error);
+                    });
+            },
+            toggleSelectAll(event) {
+                if (event.target.checked) {
+                    this.selectedProducts = this.cartProducts.map(product => ({
+                        productID: product.productID,
+                        quantity: product.quantity,
+                        total: product.total,
+                    }));
+                } else {
+                    this.selectedProducts = [];
+                }
+            },
+            VerifyCartItems() {
+                axios.get(`${BackendUrl}/ShoppingCart/${this.userID}/Verify`)
+                    .then((response) => {
+                        this.invalidItems = response.data;
+                        if (this.invalidItems.length > 0) {
+                            this.$refs.warningInvalidCartModal.openModal("Se han detectado items invalidos (Exceden el stock disponible) ¿Desea eliminarlos del carrito?");
+                        } else {
+                            this.$refs.confirmCartModal.openModal("Su carrito es valido, se puede procesar la compra");
+                        }
+                    })
+                    .catch((error) => {
+                        this.$refs.errorCartModal.openModal("Error al verificar los productos del carrito", error);
+                    });
 
-    <style scoped>
-        @import '../styles/GeneralStyle.css';
-        @import '../styles/CartStyle.css';
-    </style>
+
+            },
+            deleteInvalidItems() {
+                axios.delete(`${BackendUrl}/ShoppingCart/${this.userID}/DeleteInvalidProducts`, {
+                    data: this.invalidItems
+                }).then(() => {
+                    this.$refs.confirmCartModal.openModal("Se han eliminado los elementos invalidos del carrito");
+                    this.getUserCart();
+                })
+                    .catch((error) => {
+                        this.$refs.errorCartModal.openModal("Error al eliminar los elementos invalidos del carrito", error);
+                    });
+            }
+        },
+        computed: {
+            totalPrice() {
+                return this.cartProducts.reduce((total, product) => total + product.totalSales, 0);
+            },
+            selectedTotal() {
+                if (this.selectedProducts.length > 0) {
+                    return this.selectedProducts.reduce((total, product) => total + product.totalSales, 0);
+                }
+                return 0;
+            },
+            areAllSelected() {
+                return this.selectedProducts.length === this.cartProducts.length;
+            },
+        },
+        mounted() {
+            this.userID = this.getUserId();
+            this.getUserCart();
+        },
+        props: {
+            isAdmin: {
+                type: Boolean,
+                required: true,
+            },
+            isClient: {
+                type: Boolean,
+                required: true,
+            },
+        },
+    };
+</script>
+
+<style scoped>
+    @import '../styles/GeneralStyle.css';
+    @import '../styles/CartStyle.css';
+</style>
