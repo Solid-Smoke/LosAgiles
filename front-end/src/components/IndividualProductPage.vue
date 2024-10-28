a41c6f6fa5734988c5ae7da06af2ae564cd35281
JavierPupo <javi408x@gmail.com>
2024-10-28 17:21:33 -0600

    Se modificó la selección de cantidad para que sea un dropdown y evitar que se ingrese ua cantidad superiror al stock del producto. Además, se redirige al homepage después de agregar al carrito.

--- front-end/src/components/IndividualProductPage.vue
+++ front-end/src/components/IndividualProductPage.vue
@@ -1,189 +1,196 @@
 <template>
-    <MainNavbar />
-  
-    <b-container class="product-page">
-      <b-row class="justify-content-center">
-        <b-col lg="8" md="10" sm="12" class="text-center">
-          <img :src="getProductImage(product.productImageBase64)" alt="Product Image" class="img-fluid product-image mb-3" />
-          <h2 class="product-name">{{ product.name }}</h2>
-          <p class="product-price"><strong>Precio:</strong> &#x20a1;{{ product.price ? product.price.toFixed(2) : 'No disponible' }}</p>
-        </b-col>
-      </b-row>
-  
-      <b-row class="justify-content-center">
-        <b-col lg="8" md="10" sm="12" class="text-left">
-          <p><strong>Stock:</strong> {{ product.stock }}</p>
-          <p><strong>Peso:</strong> {{ product.weight }} kg</p>
-          <p class="product-description"><strong>Descripción:</strong> {{ product.description }}</p>
-          <p><strong>Categoría:</strong> {{ product.category }}</p>
-  
-          <div v-if="product.isPerishable">
-            <p><strong>Cantidad diaria:</strong> {{ product.dailyAmount }}</p>
-            <p><strong>Días disponibles:</strong> {{ product.daysAvailable }}</p>
-          </div>
-  
-          <p class="business-name"><strong>Emprendimiento:</strong> {{ product.businessName }}</p>
-  
-          <b-form-group label="Cantidad a ordenar:">
-            <b-form-input type="number" v-model="quantity" min="1" :max="product.stock" />
-          </b-form-group>
-  
-          <div class="text-center mt-2">
-            <template v-if="isClient">
-                <b-button variant="primary" class="add-to-cart-btn" @click="confirmAddToCart">Añadir al Carrito</b-button>
-            </template>
-            <template v-else>
-                <b-button variant="primary" class="add-to-cart-btn" @click="redirectToRegister">Registrarse en el sitio</b-button>
-            </template>
-            <b-button variant="primary" class="close-add-to-cart-btn" @click="closeCart" >Volver al Homepage</b-button>
-          </div>
-        </b-col>
-      </b-row>
-      <ActionModalConfirm ref="confirmProductModal" />
-      <ActionModalError ref="errorProductModal"/>
-      <ActionModalWarning ref="warningProductModal" @confirmed="addToShoppingCart" />
-    </b-container>
-  </template>
-  
-  <script>
-  import { BackendUrl } from '@/main';
-  import MainNavbar from './MainNavbar.vue';
-  import ActionModalConfirm from './ActionModalConfirm.vue';
-  import ActionModalError from './ActionModalError.vue';
-  import ActionModalWarning from './ActionModalWarning.vue';
-  import axios from 'axios';
-  
-  export default {
-    components: {
-      MainNavbar,
-      ActionModalConfirm,
-      ActionModalWarning,
-      ActionModalError,
-    },
-    data() {
-      return {
-        product: {},
-        quantity: 1,
-      };
-    },
-    methods: {
-      getProductImage(productImageBase64) {
-        if (!productImageBase64) {
-          return "https://imporpec.com.bo/images/" + "image_not_available.gif";
-        }
-        return `data:image/png;base64,${productImageBase64}`;
-      },
-      getProductDetails(productId) {
-        axios
-          .get(`${BackendUrl}/Products/${productId}`)
-          .then((response) => {
-            this.product = response.data;
-          })
-          .catch((error) => {
-            console.error('Error obteniendo detalles del producto:', error);
-          });
-      },
-      confirmAddToCart(){
-          this.$refs.warningProductModal.openModal("¿Deseas añadir este producto al carrito?");
-      },
-      addToShoppingCart() {
-        const user = JSON.parse(localStorage.getItem('user'));
-        const id = Number(user[0].userID);
-          axios
-              .post(`${BackendUrl}/ShoppingCart/${id}`, {
-                  productId: this.product.productID,
-                  amount: this.quantity
-              })
-              .then(() => {
-                  this.$refs.confirmProductModal.openModal("Se ha añadido el producto al carrito de forma exitosa");
-              })
-              .catch((error) => {
-                  this.$refs.errorProductModal.openModal("Error al añadir el producto al carrito", error);
-              });
-      },
-      closeCart() {
-        this.$router.push({ name: 'Home' });
-        },
-      redirectToRegister() {
-          this.$router.push('/registro');
+  <MainNavbar />
+
+  <b-container class="product-page">
+    <b-row class="justify-content-center">
+      <b-col lg="8" md="10" sm="12" class="text-center">
+        <img :src="getProductImage(product.productImageBase64)" alt="Product Image" class="img-fluid product-image mb-3" />
+        <h2 class="product-name">{{ product.name }}</h2>
+        <p class="product-price"><strong>Precio:</strong> &#x20a1;{{ product.price ? product.price.toFixed(2) : 'No disponible' }}</p>
+      </b-col>
+    </b-row>
+
+    <b-row class="justify-content-center">
+      <b-col lg="8" md="10" sm="12" class="text-left">
+        <p><strong>Stock:</strong> {{ product.stock }}</p>
+        <p><strong>Peso:</strong> {{ product.weight }} kg</p>
+        <p class="product-description"><strong>Descripción:</strong> {{ product.description }}</p>
+        <p><strong>Categoría:</strong> {{ product.category }}</p>
+
+        <div v-if="product.isPerishable">
+          <p><strong>Cantidad diaria:</strong> {{ product.dailyAmount }}</p>
+          <p><strong>Días disponibles:</strong> {{ product.daysAvailable }}</p>
+        </div>
+
+        <p class="business-name"><strong>Emprendimiento:</strong> {{ product.businessName }}</p>
+
+        <b-form-group label="Cantidad a ordenar:">
+          <b-form-select v-model="quantity" :options="quantityOptions" />
+        </b-form-group>
+
+        <div class="text-center mt-2">
+          <template v-if="isClient">
+              <b-button variant="primary" class="add-to-cart-btn" @click="confirmAddToCart">Añadir al Carrito</b-button>
+          </template>
+          <template v-else>
+              <b-button variant="primary" class="add-to-cart-btn" @click="redirectToRegister">Registrarse en el sitio</b-button>
+          </template>
+          <b-button variant="primary" class="close-add-to-cart-btn" @click="closeCart">Volver al Homepage</b-button>
+        </div>
+      </b-col>
+    </b-row>
+    <ActionModalConfirm ref="confirmProductModal"/>
+    <ActionModalError ref="errorProductModal"/>
+    <ActionModalWarning ref="warningProductModal" @confirmed="addToShoppingCart" />
+  </b-container>
+</template>
+
+<script>
+import { BackendUrl } from '@/main';
+import MainNavbar from './MainNavbar.vue';
+import ActionModalConfirm from './ActionModalConfirm.vue';
+import ActionModalError from './ActionModalError.vue';
+import ActionModalWarning from './ActionModalWarning.vue';
+import axios from 'axios';
+
+export default {
+  components: {
+    MainNavbar,
+    ActionModalConfirm,
+    ActionModalWarning,
+    ActionModalError,
+  },
+  data() {
+    return {
+      product: {},
+      quantity: 1,
+    };
+  },
+  computed: {
+    quantityOptions() {
+      return Array.from({ length: this.product.stock }, (_, i) => i + 1);
+    }
+  },
+  methods: {
+    getProductImage(productImageBase64) {
+      if (!productImageBase64) {
+        return "https://imporpec.com.bo/images/" + "image_not_available.gif";
       }
+      return `data:image/png;base64,${productImageBase64}`;
     },
-    mounted() {
-      const productId = this.$route.params.id;
-      this.getProductDetails(productId);
+    getProductDetails(productId) {
+      axios
+        .get(`${BackendUrl}/Products/${productId}`)
+        .then((response) => {
+          this.product = response.data;
+        })
+        .catch((error) => {
+          console.error('Error obteniendo detalles del producto:', error);
+        });
     },
-    props: {
-        isAdmin: {
-            type: Boolean,
-            required: true,
-        },
-        isClient: {
-            type: Boolean,
-            required: true,
-        },
+    confirmAddToCart(){
+        this.$refs.warningProductModal.openModal("¿Deseas añadir este producto al carrito?");
     },
-  };
-  </script>
-  
-  <style scoped>
-  .product-page {
-    min-height: 90vh;
-    padding: 10px 5px;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-  }
-  
-  .product-image {
-    width: 150px;
-    height: auto;
-    border-radius: 10px;
-    object-fit: cover;
-  }
-  
-  .product-name {
-    font-size: 1.5rem;
-    font-weight: bold;
-    margin-bottom: 8px;
-  }
-  
-  .product-price {
-    font-size: 1.2rem;
-    color: #333;
-    margin-bottom: 12px;
-  }
-  
-  .business-name {
-    font-size: 1rem;
-    color: #070707;
-    margin-bottom: 10px;
-  }
-  
-  .product-description {
-    text-align: justify;
-  }
+    addToShoppingCart() {
+      const user = JSON.parse(localStorage.getItem('user'));
+      const id = Number(user[0].userID);
 
-  .add-to-cart-btn {
-    background-color: #1a8654;
-    font-size: 1rem;
-    padding: 10px 20px;
-    margin: 0 10px;
-  }
+      axios
+        .post(`${BackendUrl}/ShoppingCart/${id}`, {
+            productId: this.product.productID,
+            amount: this.quantity
+        })
+        .then(() => {
+            this.$refs.confirmProductModal.openModal("Se ha añadido el producto al carrito de forma exitosa");
+            setTimeout(() => {
+                this.$router.push({ name: 'Home' });
+            }, 2000);
+        })
+        .catch(() => {
+            this.$refs.errorProductModal.openModal("Error al añadir el producto al carrito");
+        });
+    },
+    closeCart() {
+      this.$router.push({ name: 'Home' });
+    },
+    redirectToRegister() {
+        this.$router.push('/registro');
+    }
+  },
+  mounted() {
+    const productId = this.$route.params.id;
+    this.getProductDetails(productId);
+  },
+  props: {
+      isAdmin: {
+          type: Boolean,
+          required: true,
+      },
+      isClient: {
+          type: Boolean,
+          required: true,
+      },
+  },
+};
+</script>
 
+<style scoped>
+.product-page {
+  min-height: 90vh;
+  padding: 10px 5px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
 
-  .close-add-to-cart-btn {
-    background-color: #0cbde1;
-    font-size: 1rem;
-    padding: 10px 20px;
-    margin: 0 10px;
-  }
-  
-  .b-form-input {
-    font-size: 1rem;
-  }
-  
-  .product-page div {
-    margin-bottom: 3px;
-  }
-  </style>
-  
+.product-image {
+  width: 150px;
+  height: auto;
+  border-radius: 10px;
+  object-fit: cover;
+}
+
+.product-name {
+  font-size: 1.5rem;
+  font-weight: bold;
+  margin-bottom: 8px;
+}
+
+.product-price {
+  font-size: 1.2rem;
+  color: #333;
+  margin-bottom: 12px;
+}
+
+.business-name {
+  font-size: 1rem;
+  color: #070707;
+  margin-bottom: 10px;
+}
+
+.product-description {
+  text-align: justify;
+}
+
+.add-to-cart-btn {
+  background-color: #1a8654;
+  font-size: 1rem;
+  padding: 10px 20px;
+  margin: 0 10px;
+}
+
+.close-add-to-cart-btn {
+  background-color: #0cbde1;
+  font-size: 1rem;
+  padding: 10px 20px;
+  margin: 0 10px;
+}
+
+.b-form-input {
+  font-size: 1rem;
+}
+
+.product-page div {
+  margin-bottom: 3px;
+}
+</style>
