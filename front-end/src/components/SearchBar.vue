f5cf02cdfa3735dbc77d18405629f4e1e8e07ce6
Jose <jose.amadormora@ucr.ac.cr>
2024-10-28 16:25:31 -0600

    Revert "Se hicieron cambios para que el funcionamiento de la busqueda este perfecta."
    This reverts commit 6aa9ec94999080888da566bf096cfc941c45aaf4.

--- front-end/src/components/SearchBar.vue
+++ front-end/src/components/SearchBar.vue
@@ -1,98 +1,97 @@
 <template>
     <b-nav-form>
-        <b-form-input placeholder="Buscar producto, categoría o empresa..." v-model="searchText" :state="inputIsValid" aria-describedby="input-live-feedback" />
-
-        <b-button size="sm" class="my-2 my-sm-0" type="submit" @click="onSearchClick" >Buscar</b-button>
-       
-        <b-form-invalid-feedback id="input-live-feedback" style="background-color: white;" >
-            El texto tiene más de {{ maxLenghtSearchText }} caracteres o se
-            ingresó algún caracter especial no permitido. Caracteres especiales
+        <b-form-input
+            placeholder="Buscar producto, categoría o empresa..."
+            v-model="searchText" :state="inputIsValid"
+            aria-describedby="input-live-feedback"/>
+        <b-button size="sm" class="my-2 my-sm-0" type="submit" 
+            @click="searchProducts(this.startSearchIndex,
+                this.maxResults, this.searchText)">Buscar</b-button>
+    
+        <b-form-invalid-feedback id="input-live-feedback"
+        style="background-color: white;">
+            El texto tiene más de {{ maxLenghtSearchText }} caracteres
+            o se ingresó algún caracter especial no permitido. Caracteres especiales
             permitidos: {{ allowedSpecialCharacters }}
         </b-form-invalid-feedback>
     </b-nav-form>
+    
 </template>
 
 <script>
-import { BackendUrl } from "@/main";
-import axios from "axios";
-
-export default {
-    props: {
-        startSearchIndex: Number,
-        maxResults: Number,
-    },
-    watch: {
-        startSearchIndex() {
-            this.searchProducts(this.startSearchIndex, this.maxResults, this.searchText);
+import { BackendUrl } from '@/main';
+import axios from 'axios';
+    export default {
+        props: {
+            startSearchIndex: Number,
+            maxResults: Number,
         },
-        searchText() {
-            this.$emit("searchMade");
-        },
-    },
-    data() {
-        return {
-            products: [],
-            searchText: "",
-            maxLenghtSearchText: 50,
-            allowedSpecialCharacters: "-+_$/&#",
-        };
-    },
-    computed: {
-        inputIsValid() {
-            let validationRegex = new RegExp("^[a-zA-Z0-9-+_$/&#\\sÀ-ÖØ-öø-ÿ]*$");
-            return (
-                this.searchText.length <= this.maxLenghtSearchText &&
-                validationRegex.test(this.searchText)
-            );
-        },
-    },
-    methods: {
-        onSearchClick() {
-            this.$emit("searchMade");
-            this.searchProducts(0, this.maxResults, this.searchText);
-        },
-        searchProducts(startIndex, maxResults, searchText) {
-            if (this.inputIsValid) {
-                axios
-                    .get(`${BackendUrl}/Products`, {
-                        params: {
-                            startIndex,
-                            maxResults,
-                            searchText: searchText.trim() || "",
-                        },
-                    })
-                    .then((response) => {
-                        this.products = response.data.map((product) => ({
-                            ...product,
-                            productID: product.productID,
-                        }));
-
-                        this.$emit("productsRetrieved", this.products);
-                        this.countProductsBySearch(searchText);
-                    })
-                    .catch((error) => {
-                        console.error("Error en la búsqueda de productos:", error);
-                    });
+        watch: {
+            startSearchIndex: function() {
+                this.searchProducts(this.startSearchIndex,
+                    this.maxResults, this.searchText)
+            },
+            searchText: function () {
+                this.$emit('searchMade');
             }
         },
-        countProductsBySearch(searchText) {
-            axios
-                .get(`${BackendUrl}/Products/CountProductsBySearch`, {
-                    params: { searchText: searchText.trim() || "" },
-                })
-                .then((response) => {
-                    this.$emit("productsCounted", response.data);
-                })
-                .catch((error) => {
-                    console.error("Error contando productos:", error);
-                });
+        data() {
+            return {
+                products: [],
+                searchText: "",
+                maxLenghtSearchText: 50,
+                allowedSpecialCharacters: "-+_$/&#"
+            };
         },
-    },
-    mounted() {
-        this.searchProducts(0, this.maxResults, "");
-    },
-};
+        computed: {
+            inputIsValid() {
+                let validationRegex =
+                new RegExp('^[a-zA-Z0-9-+_$/&#\\sÀ-ÖØ-öø-ÿ]*$');
+                return (this.searchText.length <= this.maxLenghtSearchText
+                    && validationRegex.test(this.searchText))
+            }
+        },
+        methods: {
+            searchProducts(startIndex, maxResults, searchText) {
+                if(this.inputIsValid) {
+                    axios
+                    .get(BackendUrl +
+                        "/Products", {params: {
+                            startIndex,
+                            maxResults,
+                            searchText: searchText.trim()
+                        }})
+                    .then(
+                        (response) => {
+                            this.countProductsBySearch(this.searchText);
+                            this.products = response.data;
+                            this.$emit('productsRetreived', this.products);
+                        })
+                    .catch(function (error) {
+                        console.log(error);
+                    });
+                }
+            },
+            countProductsBySearch(searchText) {
+                axios
+                .get(BackendUrl +
+                    "/Products/CountProductsBySearch",
+                    {params: {searchText}})
+                .then(
+                    (response) => {
+                        this.$emit('productsCounted', response.data);
+                    })
+                .catch(function (error) {
+                    console.log(error);
+                });
+            },
+        },
+        mounted() {
+            this.searchProducts(0, this.maxResults, "");
+        }
+    }
 </script>
 
 <style scoped>
+
 </style>
