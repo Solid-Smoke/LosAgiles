038ba74635cf5cb7941952dd7c30d70dd39903e7
Jose <jose.amadormora@ucr.ac.cr>
2024-10-28 21:13:46 -0600

    comentario innecesario

--- front-end/src/components/CreateOrder.vue
+++ front-end/src/components/CreateOrder.vue
@@ -230,7 +230,7 @@
             }
             const today = new Date();
             const yyyy = today.getFullYear();
-            const mm = String(today.getMonth() + 1).padStart(2, '0'); // Meses comienzan en 0
+            const mm = String(today.getMonth() + 1).padStart(2, '0');
             const dd = String(today.getDate()).padStart(2, '0');
             this.todayDate = `${yyyy}-${mm}-${dd}`;
         },
