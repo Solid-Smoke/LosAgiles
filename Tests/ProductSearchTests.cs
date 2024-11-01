9304028eeb41e40e424b7d6619cd6dabbbb8827a
Jose <jose.amadormora@ucr.ac.cr>
2024-10-31 23:22:53 -0600

    Se añade test de countProductsBySearch

---
+++ Tests/ProductSearchTests.cs
@@ -0,0 +1,31 @@
+﻿
+using back_end.Application.Interfaces;
+using back_end.Application.Queries;
+using Moq;
+
+namespace Tests
+{
+    internal class ProductSearchTests
+    {
+        [Test]
+        public void countProductsBySearchWorksWithNullSearchText()
+        {
+            var productHandler = new Mock<IProductHandler>();
+            const int productHandlermockReturnValue = 3;
+            productHandler.Setup(handler =>
+                handler.countProductsBySearch(It.IsAny<string>())).Returns(productHandlermockReturnValue);
+            var productQuery = new ProductQuery(productHandler.Object);
+
+            try
+            {
+                int productCount = productQuery.countProductsBySearch(null);
+                Assert.AreEqual(productCount, productHandlermockReturnValue);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail();
+            }
+
+        }
+    }
+}
