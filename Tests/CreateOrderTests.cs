385f2212d695dd2cd334ecf8441913c51c6cb16f
Jose <jose.amadormora@ucr.ac.cr>
2024-11-01 00:41:27 -0600

    Se añade prueba para verificar que sucede si se intenta realizar una orden con una lista de productos nula

---
+++ Tests/CreateOrderTests.cs
@@ -0,0 +1,38 @@
+﻿
+
+using back_end.Application.Commands;
+using back_end.Application.Interfaces;
+using back_end.Domain;
+using Moq;
+
+namespace Tests
+{
+    internal class CreateOrderTests
+    {
+        Mock<IOrderHandler> orderHandlerMock;
+        SubmitOrder orderSubmitter;
+        [SetUp]
+        public void SetUp()
+        {
+            orderHandlerMock = new Mock<IOrderHandler>();
+            orderSubmitter = new SubmitOrder(orderHandlerMock.Object);
+        }
+        [Test]
+        public void CreateOrderDoesntWorkWithNullProductList()
+        {
+            var orderData = new CreateOrderModel();
+            orderData.Products = null;
+            
+            try
+            {
+                orderSubmitter.createOrder(orderData);
+            } catch
+            {
+                Assert.Pass();
+            }
+            Assert.Fail();
+
+        }
+
+    }
+}
