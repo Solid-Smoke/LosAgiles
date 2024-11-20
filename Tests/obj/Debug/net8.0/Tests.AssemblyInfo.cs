a19ab143546b3d6fded685d155e196a586f09820
ANDREW.ROSALES <ANDREW.ROSALES@ucr.ac.cr>
2024-11-19 18:58:45 -0600

    Test del Backend para ordenes completadas. (Incompletos)

--- Tests/obj/Debug/net8.0/Tests.AssemblyInfo.cs
+++ Tests/obj/Debug/net8.0/Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+264d743b8f1974f9c499939fd2647f482bbc3839")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4dc3d2e15584c9f443c3122c30eb38113770c156")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
