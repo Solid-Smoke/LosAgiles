4185151b7160ec9406975527d583867f65f5b8fb
Solid-Smoke <pablo.cascantetorres@ucr.ac.cr>
2024-11-21 12:04:27 -0600

    Backend entero de reporte de todas las ordenes pendientes para vista de los administradores, adem'as de poder descargar a pdf

---
+++ back_end/Application/Commands/GenerateAllPendingOrderReportPDF.cs
@@ -0,0 +1,44 @@
+﻿using back_end.Application.Interfaces;
+using back_end.Application.Reports;
+using back_end.Domain;
+
+namespace back_end.Application.Commands
+{
+    public class GenerateAllPendingOrderReportPDF
+    {
+        private readonly PendingOrderReport pendingOrderReport;
+        public GenerateAllPendingOrderReportPDF(
+            IReportHandler reportHandler)
+        {
+            pendingOrderReport = new PendingOrderReport(reportHandler);
+        }
+
+        public byte[] Execute(ReportBaseFilters baseFilters)
+        {
+            if (baseFilters == null)
+                throw new ArgumentNullException(nameof(baseFilters), "Base filters cannot be null.");
+            if (baseFilters.StartDate > baseFilters.EndDate)
+                throw new ArgumentException("StartDate cannot be later than EndDate.", nameof(baseFilters));
+            List<AdminReportOrderData> reportData = pendingOrderReport.FetchReportOrders(baseFilters);
+
+            if (reportData.Count > 0)
+            {
+                string orderIDs = pendingOrderReport.GetOrderIDsFromReport(reportData.AsEnumerable());
+                List<ReportOrderProductData> orderProducts = pendingOrderReport.FetchOrderProducts(orderIDs);
+                foreach (var pendingOrder in reportData)
+                {
+                    var productsForCurrentOrder = orderProducts
+                        .Where(product => product.OrderID == pendingOrder.OrderID)
+                        .ToList();
+
+                    pendingOrder.BusinessName = string.Join(", ", productsForCurrentOrder
+                        .Select(product => product.BusinessName)
+                        .Distinct());
+
+                    pendingOrder.Amount = productsForCurrentOrder.Sum(product => product.Amount);
+                }
+            }
+            return pendingOrderReport.GeneratePdf(reportData);
+        }
+    }
+}
