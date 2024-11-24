9c7b383529ae72071d91cc2e06842e7d2e304859
Solid-Smoke <pablo.cascantetorres@ucr.ac.cr>
2024-11-23 19:03:08 -0600

    Tests completos y funcionales

---
+++ Tests/GenerateAllCancelledOrderReportPDFTests.cs
@@ -0,0 +1,41 @@
+﻿using back_end.Application.Commands;
+using back_end.Application.Interfaces;
+using back_end.Application.Reports;
+using back_end.Domain;
+using Moq;
+
+namespace Tests {
+    internal class GenerateAllCancelledOrderReportPDFTests {
+        private Mock<IReportHandler> _mockReportHandler;
+        private GenerateAllCancelledOrderReportPDF _command;
+        private Mock<AllCancelledOrderReport> _mockAllCancelledOrderReport;
+
+        [SetUp]
+        public void SetUp() {
+            _mockReportHandler = new Mock<IReportHandler>();
+            _mockAllCancelledOrderReport = new Mock<AllCancelledOrderReport>(_mockReportHandler.Object);
+            _command = new GenerateAllCancelledOrderReportPDF(_mockAllCancelledOrderReport.Object);
+        }
+
+        [Test]
+        public void Execute_ShouldThrowArgumentNullException_WhenFiltersAreNull() {
+            // Arrange
+            ReportBaseFilters? filters = null;
+            // Act & Assert
+            var ex = Assert.Throws<ArgumentNullException>(() => _command.Execute(filters));
+        }
+
+        [Test]
+        public void Execute_ShouldThrowArgumentException_WhenStartDateIsAfterEndDate() {
+            // Arrange
+            var filters = new ReportBaseFilters {
+                ClientID = 0,
+                StartDate = DateTime.Now.AddDays(1),
+                EndDate = DateTime.Now
+            };
+
+            // Act & Assert
+            var ex = Assert.Throws<ArgumentException>(() => _command.Execute(filters));
+        }
+    }
+}
