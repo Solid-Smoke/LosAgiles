90fc334edf09eb60c6d44e3565ce7440e9da439a
ANDREW.ROSALES <ANDREW.ROSALES@ucr.ac.cr>
2024-11-25 18:19:15 -0600

    Correccion de script para obtener ganancias

--- scripts/021-FuncionParaObtenerGananciasTotales.sql
+++ scripts/021-FuncionParaObtenerGananciasTotales.sql
@@ -13,23 +13,23 @@ RETURN
     SELECT 
         [b].[Name] AS [BusinessName],
         FORMAT([o].[StatusChangeDate], 'MM') AS [Month],
-        SUM([o].[SubtotalCost]) AS [TotalPurchase],
+        SUM([op].[Amount] * [op].[PriceAtTimeOfPurchase]) AS [TotalPurchase], 
         SUM([o].[DeliveryCost]) AS [DeliveryCost],
-        SUM([o].[TotalCost]) AS [TotalCost]
+        SUM([o].[TotalCost]) AS [TotalCost] 
     FROM 
         [dbo].[BusinessOrders] [bo]
     INNER JOIN 
         [dbo].[Orders] [o] ON [bo].[OrderID] = [o].[OrderID]
     INNER JOIN 
         [dbo].[Businesses] [b] ON [bo].[BusinessID] = [b].[BusinessID]
+    INNER JOIN
+        [dbo].[OrderProducts] [op] ON [o].[OrderID] = [op].[OrderID]
     WHERE 
-        [o].[Status] = 'Completada'
-        AND YEAR([o].[StatusChangeDate]) = @Year
-        AND (@BusinessID IS NULL OR [b].[BusinessID] = @BusinessID)
+        [o].[Status] = 'Completada' 
+        AND YEAR([o].[StatusChangeDate]) = @Year 
+        AND (@BusinessID IS NULL OR [b].[BusinessID] = @BusinessID) 
     GROUP BY 
-		[b].[Name],
-		[o].[StatusChangeDate]
-		
-        
+        [b].[Name],
+        FORMAT([o].[StatusChangeDate], 'MM') 
 );
 GO
